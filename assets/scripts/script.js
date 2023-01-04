57cb456f87f81425378c3b47e41f980d845af427
unknown <cuberkuber@gmail.com>
2023-01-05 01:53:16 +0700

    moved color list function to different js file

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -121,49 +121,6 @@ function loadLastSelectedColor() {
     joe.set(selectedLastTime);
 };
 
-// Color List.
-
-const jsonColor = "assets/css-color-names.json";
-
-const tableBasic = document.querySelector(".__basic-colors table");
-const tableExtended = document.querySelector(".__extended-colors table");
-
-fetch(jsonColor)
-    .then(response => response.json())
-    .then(data => generateColorList(data))
-    .catch(err => {
-        tableBasic.innerHTML = err;
-        tableExtended.innerHTML = err;
-});
-
-function generateColorList(data) {
-    let colorBasic = Object.keys(data.basic);
-    let hexBasic = Object.values(data.basic);
-
-    let colorExtended = Object.keys(data.extended);
-    let hexExtended = Object.values(data.extended);
-
-    // Basic.
-    colorBasic.forEach((item, index) => {
-        let colorTable = `<tr>
-                            <td>${item}</td>
-                            <td><button style="background: ${hexBasic[index]};" data-color="${hexBasic[index]}" onclick="selectColor(this.dataset.color)"></button></td>
-                        </tr>`;
-
-        tableBasic.innerHTML += colorTable;
-    });
-
-    // Extended.
-    colorExtended.forEach((item, index) => {
-        let colorTable = `<tr>
-                            <td>${item}</td>
-                            <td><button style="background: ${hexExtended[index]};" data-color="${hexExtended[index]}" onclick="selectColor(this.dataset.color)"></button></td>
-                        </tr>`;
-
-        tableExtended.innerHTML += colorTable;
-    });
-};
-
 hexInput.addEventListener("change", () => setColor(hexInput));
 rgbInput.addEventListener("change", () => setColor(rgbInput));
 hexCopy.addEventListener("click", () => copyText(hexInput));
